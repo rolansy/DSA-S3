d07c2d6e7bcbee28870e2acf0cd21e95737c38b8
rolansy <ronalshoeygeorge@gmail.com>
2023-10-05 02:37:09 +0530

    Some Minor Changes

--- Polynomial Trial.c
+++ Polynomial Trial.c
@@ -3,13 +3,13 @@
 struct poly{
     int e;
     int c;
-}
+};
 
 //Function Declaration
-void readpoly(struct poly p,int n);
-void sortpoly(struct poly p,int n);
-void printpoly(struct poly p,int n);
-void addpoly(struct poly p1,struct poly res,int n1,int n2);
+void readpoly(struct poly *p,int n);
+void sortpoly(struct poly *p,int n);
+void printpoly(struct poly *p,int n);
+void addpoly(struct poly *p1,struct poly *p2,struct poly *res,int n1,int n2);
 
 void main(){
     struct poly p1[10],p2[10],res[10];
@@ -36,25 +36,25 @@ void main(){
 }
 
 //read polynomials
-void readpoly(struct poly p,int n){
+void readpoly(struct poly *p,int *n){
     printf("Enter Number of Terms : ");
     scanf("%d",&n);
-    for (int i; i<n; i++){
-        printf("Enter the Coefficient : ")
-        scanf("%d",&p[i].c)
-        printf("Enter the Exponent : ")
-        scanf("%d",&p[i].e)
+    for (int i; i<*n; i++){
+        printf("Enter the Coefficient : ");
+        scanf("%d",&p[i].c);
+        printf("Enter the Exponent : ");
+        scanf("%d",&p[i].e);
     } 
 }
 
 //print polynomials
-void printpoly(struct poly p,int n){
+void printpoly(struct poly *p,int n){
     for (int i; i<n; i++){
         if (p[i].c==0){
             continue;
         }
-        if (p[i]e==0]){
-            printf("%d",p[i].c)
+        if (p[i]e==0){
+            printf("%d",p[i].c);
         }
         else{
             printf("%dx^%d",p[i].c,p[i].e);
@@ -67,7 +67,7 @@ void printpoly(struct poly p,int n){
 }
 
 //Sort Polynomials In Descending Order
-void sortpoly(struct poly p,int n){
+void sortpoly(struct poly *p,int n){
     for(int i=0;i<n-1,i++){
         for(int j=0;i<n-i-j,j++){
             if(p[j].e<p[j+1].e){
