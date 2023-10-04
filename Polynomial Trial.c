0f82d09c3cebed5106c000171ca5305b2cb0acac
rolansy <ronalshoeygeorge@gmail.com>
2023-10-05 02:51:40 +0530

    Minor Fixes

--- Polynomial Trial.c
+++ Polynomial Trial.c
@@ -6,12 +6,12 @@ struct poly{
 };
 
 //Function Declaration
-void readpoly(struct poly *p,int n);
+void readpoly(struct poly *p,int *n);
 void sortpoly(struct poly *p,int n);
 void printpoly(struct poly *p,int n);
-void addpoly(struct poly *p1,struct poly *p2,struct poly *res,int n1,int n2);
+int addpoly(struct poly *p1,struct poly *p2,struct poly *res,int n1,int n2);
 
-void main(){
+int main(void){
     struct poly p1[10],p2[10],res[10];
     int n1, n2, n3;
     
@@ -26,7 +26,7 @@ void main(){
     printf("Polynomial 2 : ");
     printpoly(p2,n2);
 
-    n3=add_poly(p1,p2,res,n1,n2);
+    n3=addpoly(p1,p2,res,n1,n2);
     printf("Resultant Polynomial : ");
     printpoly(res,n3);
 
@@ -38,7 +38,7 @@ void main(){
 //read polynomials
 void readpoly(struct poly *p,int *n){
     printf("Enter Number of Terms : ");
-    scanf("%d",&n);
+    scanf("%d",n);
     for (int i; i<*n; i++){
         printf("Enter the Coefficient : ");
         scanf("%d",&p[i].c);
@@ -71,9 +71,9 @@ void sortpoly(struct poly *p,int n){
     for(int i=0;i<n-1;i++){
         for(int j=0;i<n-i-j;j++){
             if(p[j].e<p[j+1].e){
-                struct poly temp=p[j].e;
-                p[j].e=p[j+1].e;
-                p[j+1].e=temp;
+                struct poly temp=p[j];
+                p[j]=p[j+1];
+                p[j+1]=temp;
             }
         }
     }
