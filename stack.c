13353eb45feae94ba823b1c46aa30b3040696561
rolansy <ronalshoeygeorge@gmail.com>
2023-10-19 02:15:31 +0530

    Some Fixes Stack

--- stack.c
+++ stack.c
@@ -1,35 +1,11 @@
 #include <stdio.h>
 int top=-1;
-void push(int s[100];int x;int n){
-    if (top==n-1){
-        printf("Stack is Full\n");
-    }
-    else{
-        s[++top]=x;
-    }
-}
-void pull(int s[100]){
-    if (n==-1){
-        printf("Stack is Empty\n");
-    }
-    else{
-        printf("Popped Element : %d\n",s[top--]);
-        display(s);
-    }
-}
-void display(int s[100]){
-    if (top=-1){
-        printf("Stack is Empty\n");
-    }
-    else{
-        printf("Stack Elements : \n");
-        for(i=0;i<=top;i++){
-            printf("%d\n",s[i]);
-        }
-    }
-}
+void push(int s[100],int x,int n);
+void pop(int s[100]);
+void display(int s[100]);
 void main(){
-    int s[100],n,x,i,c,a=0;
+    int top=-1;
+    int s[100],n,x,c,a=0;
     printf("Enter Stack Size : ");
     scanf("%d",&n);
     while (a==0){
@@ -38,11 +14,42 @@ void main(){
         scanf("%d",&c);
         switch (c){
             case 1:
+                printf("Enter Element to be Pushed : ");
+                scanf("%d",&x);
                 push(s,x,n);
                 display(s);
                 break;
             case 2:
-                pull(s);
+                pop(s);
+                display(s);
+                break;
+        }
+    }
+}
+void push(int s[100],int x,int n){
+    if (top==n-1){
+        printf("Stack is Full\n");
+    }
+    else{
+        s[++top]=x;
+    }
+}
+void pop(int s[100]){
+    if (top==-1){
+        printf("Stack is Empty\n");
+    }
+    else{
+        printf("Popped Element : %d\n",s[top--]);
+    }
+}
+void display(int s[100]){
+    if (top==-1){
+        printf("Stack is Empty\n");
+    }
+    else{
+        printf("Stack Elements : \n");
+        for(int i=0;i<=top;i++){
+            printf("%d\n",s[i]);
         }
     }
 }
