00d84ae3b81b6aeca9c6dfd3f82f3c02376ea49a
rolansy <ronalshoeygeorge@gmail.com>
2023-10-19 01:50:34 +0530

    Main stack pull and push added

--- stack.c
+++ stack.c
@@ -14,5 +14,35 @@ void pull(int s[100]){
     }
     else{
         printf("Popped Element : %d\n",s[top--]);
+        display(s);
+    }
+}
+void display(int s[100]){
+    if (top=-1){
+        printf("Stack is Empty\n");
+    }
+    else{
+        printf("Stack Elements : \n");
+        for(i=0;i<=top;i++){
+            printf("%d\n",s[i]);
+        }
+    }
+}
+void main(){
+    int s[100],n,x,i,c,a=0;
+    printf("Enter Stack Size : ");
+    scanf("%d",&n);
+    while (a==0){
+        printf("1.Push\n2.Pull\n3.Display\n4.Exit");
+        printf("Enter Choice");
+        scanf("%d",&c);
+        switch (c){
+            case 1:
+                push(s,x,n);
+                display(s);
+                break;
+            case 2:
+                pull(s);
+        }
     }
 }
