f53b3b4f6db06637dcf0f364ef780e582febbf89
rolansy <ronalshoeygeorge@gmail.com>
2023-11-02 02:08:04 +0530

    insert queue

--- Normal Queue.c
+++ Normal Queue.c
@@ -1,2 +1,19 @@
 #include <stdio.h>
 int a[20],n,f,r;
+void insert(int x)
+{
+    if(r==n-1)
+    {
+        printf("Queue is full\n");
+    }
+    else if(r==-1)
+        {
+            f=0;
+            r=0;
+        }
+    else{
+        r++;
+        a[r]=x;
+    }
+    
+}
