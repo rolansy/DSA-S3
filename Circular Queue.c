ae95a84db5a678448d24a8fb2c0896802c8dae4c
rolansy <ronalshoeygeorge@gmail.com>
2023-11-02 08:24:59 +0530

    del funch

--- Circular Queue.c
+++ Circular Queue.c
@@ -7,7 +7,28 @@ void insert(int x){
     {
         printf("Queue is full\n");
     }
+    else if(r==-1){
+        f=0;
+        r=0;
+        q[r]=x;
+    }
     else{
         q[r]=x;
+    }
+}
+
+void delete(int x){
+    if (f==-1){
+        printf("Queue is empty\n")
+    }
+    else if (f==r)
+    {
+        x=q[f];
+        f=-1;
+        r=-1;
+    }
+    else{
+        x=q[f];
+        f=(f+1)%n;
     }
 }
