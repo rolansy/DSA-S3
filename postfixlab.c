3948c5a88e3b2edbde205c3549975bff921c7ea5
Ronal <128417944+rolansy@users.noreply.github.com>
2023-11-02 11:04:40 +0530

    Update postfixlab.c operand corrections and output perfected

--- postfixlab.c
+++ postfixlab.c
@@ -1,9 +1,9 @@
 #include  <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <ctype.h>
 
 void post(char inf[100],char postf[100]);
-/*int isop(char c);*/
 int val(char c);
 void main(){
 	char inf[100];
@@ -11,10 +11,8 @@ void main(){
 	
 	printf("Enter Infix Expression : ");
 	scanf("%s", inf);
-	//printf("%s", inf);
-	
 	post(inf, postf);
-    	printf("Postfix expression: %s\n", postf);
+    	printf("\nPostfix expression: %s\n", postf);
 }
 
 void post(char inf[100],char postf[100]){
@@ -22,27 +20,36 @@ void post(char inf[100],char postf[100]){
 	int top=-1;
 	int j=0;
 	int n = (int)strlen(inf);
-	printf("%d", n);
-	
 	
 	for(int i=0;i<n;i++){
-	
 		char c=inf[i];
-	//	printf("%s", "testing");
-		
-		if ( c>='0' && c<='9'){
+		printf("Character : %c  ",c);
+		printf("Stack : ");
+		for(int m=0;m<(int)strlen(postf);m++){
+			printf("%c ",postf[m]);
+			}
+		printf("\n");
+		if ( isalpha(c)){
 			postf[j++]=c;
+			continue;
 		}
 		else if (c=='('){
 			s[++top]=c;
+			continue;
 		}
 		else if (c==')'){
 			while (top!=-1 && s[top]!='('){
 				postf[j++]=s[top--];
+				continue;
 			}
 			if (top!=-1 && s[top]!='('){
 				top--;
+				continue;
 			}
+		}
+		else if(c=='^'){
+			s[++top]=c;
+			continue;
 		}
 		else if (c=='+' || c=='-' || c=='/' || c=='*'){
 			while(top!=-1 && val(s[top])>=val(c)){
@@ -50,20 +57,19 @@ void post(char inf[100],char postf[100]){
 			}
 		s[++top]=c;
 		}
-		
 	
 	}
 		
 	while(top!=-1){
-		postf[j++]=s[top--];
+		if(s[top]!='('){
+			postf[j++]=s[top--];
+		}
+		else{
+			s[top--];
+		}
+		
 	}
-	printf("%d", j);
 }
-/*
-int isop(char c){
-	return (c=="+" || c=="-" || c=="/" || c=="*");
-}
-*/
 int val(char c){
 	if (c=='+' || c=='-'){
 		return 1;
@@ -71,7 +77,14 @@ int val(char c){
 	else if (c=='/' || c=='*'){
 		return 2;
 	}
+	else if (c=='^'){
+		return 3;
+	}
 	else {
 		return 0;
 	}
+	
+	
 }
+
+
