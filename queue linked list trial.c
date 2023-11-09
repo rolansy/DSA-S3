e0b517d6325d2c1805b05c8f66197bc3b1d2423d
rolansy <ronalshoeygeorge@gmail.com>
2023-11-09 08:17:18 +0530

    main

--- queue linked list trial.c
+++ queue linked list trial.c
@@ -55,4 +55,26 @@ void deq(){
     }
 }
 
+void main(){
+    int opt,x;
+    do{
+        printf("\n\n1.Enqueue\n2.Dequeue\n3.Display\n4.Exit\n");
+        printf("Enter your choice\n");
+        scanf("%d",&opt);
+        switch(opt){
+            case 1:printf("Enter the element to be inserted\n");
+                    scanf("%d",&x);
+                    enq(item);
+                    break;
+            case 2:deq();
+                    break;
+            case 3:display();
+                    break;
+            case 4:exit(0);
+                    break;
+            default:printf("Invalid choice\n");
+        }
+    }while(opt!=4);
+}
+
 
