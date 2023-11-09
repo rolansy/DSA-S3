82e927101fc090db9b86eee52db65fcf368b106d
rolansy <ronalshoeygeorge@gmail.com>
2023-11-09 07:58:42 +0530

    dispplay

--- stack linked list trial.c
+++ stack linked list trial.c
@@ -5,11 +5,26 @@ struct node{
 
 struct node *top;
 
+void display(){
+    struct node *ptr;
+    if(top==NULL){
+        printf("Stack is Empty\n");
+    }
+    else{
+        ptr=top;
+        printf("Stack elements are  :");
+        while(ptr!=NULL){
+            printf("%d ",ptr->data);
+            ptr=ptr->link;
+        }
+    }
+}
+
 void push(int item){
     struct node *new;
     new=(struct node *)malloc(sizeof(struct node));
     new->data=item;
     new->link=top;
     top=new;
-    display());
+    display();
 }
